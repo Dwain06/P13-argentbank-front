c311cd4a1d99def5e933ef580e0b3f9d7ca256e0
Dwain06 <dwain.mobile@gmail.com>
2022-11-25 16:32:01 +0100

    fix(profile.js): fix get token

--- src/utils/profile.js
+++ src/utils/profile.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 
 export async function getProfile() {
-    axios.defaults.headers['Authorization'] = 'Bearer ' + sessionStorage.getItem('authToken');
+    let token = getToken();
+    axios.defaults.headers['Authorization'] = 'Bearer ' + token;
     
     return axios
     .post('http://localhost:3001/api/v1/user/profile')
     .then(response => console.log(response.data.body))
     .catch(function (error) {
-        alert(error);})
+        console.log(error);})
+}
+
+function getToken(){
+    if (localStorage.getItem('authToken')) {
+        return localStorage.getItem('authToken');
+    } else if (sessionStorage.getItem('authToken')) {
+        return sessionStorage.getItem('authToken');
+    } else {
+        return undefined;
+    }
 }
